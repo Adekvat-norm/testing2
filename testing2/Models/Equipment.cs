c1511a5b9cd1f99c98ce13c8361410eeafe5304a
Денис Талышкин <denis.talyshkin@mail.ru>
2024-07-03 11:17:18 +0700

    Добавьте файлы проекта.

---
+++ testing2/Models/Equipment.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Testing.Models
+{
+    public class Equipment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Название компонента")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Название компонента")]
+        public int SerialNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Название компонента")]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required]
+        [Display(Name = "Название компонента")]
+        public int IdSupplier {  get; set; }
+
+        [Required]
+        [Display(Name = "Название компонента")]
+        public int NumberContract { get; set; }
+
+        [Required]
+        [Display(Name = "Название компонента")]
+        public int InventoryNumber {  get; set; }
+    }
+}
